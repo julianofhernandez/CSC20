7e77ef9902ca76f20dd89d479545a3fd46cd7a73
Julian Hernandez <julianofhernandez@gmail.com>
2021-02-22 11:12:13 -0800

    Fixed javadoc errors

--- lab4/RandomWalker.java
+++ lab4/RandomWalker.java
@@ -14,8 +14,6 @@ public class RandomWalker {
 	/**
 	* <p>The constructor sets all starting values to 0
 	* </p>
-	* @param None
-	* @returns None
 	*/
 	public RandomWalker() {
 		this.x = 0;
@@ -26,8 +24,6 @@ public class RandomWalker {
 	/**
 	* <p>Instructs this random walker to update its coordinates by randomly making one of the 4 possible moves (up, down, left, or right).
 	* </p>
-	* @param None
-	* @returns void
 	*/
 	public void move() {
 		Random rand = new Random();
@@ -50,7 +46,6 @@ public class RandomWalker {
 	* <p>Returns this random walker's current x-coordinate.
 	* </p>
 	* @return the walk's current x-coordinate
-	* @param none
 	*/
 	public int getX() {
 		return this.x;
@@ -60,7 +55,6 @@ public class RandomWalker {
 	* <p>Returns this random walker's current y-coordinate.
 	* </p>
 	* @return the walk's current y-coordinate
-	* @param None
 	*/
 	public int getY() {
 		return this.y;
@@ -70,7 +64,6 @@ public class RandomWalker {
 	* <p>Returns the total steps taken
 	* </p>
 	* @return The total steps this walker has taken
-	* @param None
 	*/
 	public int getSteps() {
 		return this.steps;
